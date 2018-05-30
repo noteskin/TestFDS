28a5ea3d0e99e86485d74a550578caeb9b9f6321
noteskin <sufee18@163.com>
2018-05-30 13:30:16 +0800

    服务管理接口

---
+++ src/FDS_Impl/PackageImpl.java
@@ -0,0 +1,15 @@
+public interface PackageImpl {
+
+    /**
+     * 打开“服务包”页面
+     * @param url
+     */
+    public void jumpPackageUrl(String url);
+
+    /**
+     * 【新增】服务包
+     * @param arr
+     * @param length
+     */
+    public void addPackage(String arr[][],int length);
+}
