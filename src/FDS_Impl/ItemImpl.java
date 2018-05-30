8fc14587357caa000656daa30f990ef956368391
noteskin <sufee18@163.com>
2018-05-30 13:29:57 +0800

    项目管理接口

---
+++ src/FDS_Impl/ItemImpl.java
@@ -0,0 +1,17 @@
+public interface ItemImpl {
+
+    /**
+     * 打开“项目管理”页面
+     * @param url
+     */
+    public void jumpItemUrl(String url);
+
+    /**
+     *
+     * @param arr
+     * @param length
+     */
+    public void addItem(String arr[][],int length);
+
+
+}
